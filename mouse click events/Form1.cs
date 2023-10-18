b81837a4e26e26909087f3f2373072a45f76975c
Panaria <baranyldzby@gmail.com>
2023-10-18 22:02:32 +0300

    Add project files.

---
+++ mouse click events/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mouse_click_events
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Purple;
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button1.BackColor = Color.Black;
+        }
+    }
+}
